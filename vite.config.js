d9e445dfaa6f8b1fcc8c91610498cdfbd617a0ff
Turki Aljawder <turkybh@gmail.com>
2026-02-22 10:06:25 +0300

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  // IMPORTANT: repo name here
+  base: "/gpic-dashboard/",
+});
